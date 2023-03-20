954cc1e498072b87a48aea0ad287f7beedc69d55
freezone <frison.enrico@gmail.com>
2023-03-20 18:02:34 +0000

    20230320 partial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    { id: 12, name: 'Dr Nice'},
+    { id: 13, name: 'Bombasto'},
+    { id: 14, name: 'Celeritas'},
+    { id: 15, name: 'Magneta'},
+    { id: 16, name: 'Donald Duck'},
+    { id: 17, name: 'Pluto'},
+    { id: 18, name: 'Diabolik'},
+    { id: 19, name: 'Dylan Dog'},
+    { id: 19, name: 'Hulk'}
+];
